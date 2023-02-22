ee638f96934920cd8d2dba7bec199a3f7cb5233d
Peter Korkinov <73311957+peter-korkinov@users.noreply.github.com>
2023-02-22 23:21:34 +0200

    Creating Company model

--- companies/models.py
+++ companies/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    logo = models.FileField(upload_to='logos/')
+    description = models.TextField(max_length=300)
+
+    def __str__(self):
+        return f'{self.pk} - {self.name}'
